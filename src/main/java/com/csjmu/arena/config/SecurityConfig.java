384117d2916b4ede5b29a0371b4b3af5297fec60
Shubh Maurya <shubhmaurya040@gmail.com>
2026-07-03 00:14:00 +0530

    feat: complete JWT authentication module

--- src/main/java/com/csjmu/arena/config/SecurityConfig.java
+++ src/main/java/com/csjmu/arena/config/SecurityConfig.java
@@ -7,9 +7,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import com.csjmu.arena.security.JwtFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,10 +30,20 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers("/", "/api/users/register").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/api/users/register",
+                                "/api/auth/login"
+                        ).permitAll()
 
                         .anyRequest().authenticated()
                 )
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class)
 
                 .httpBasic(Customizer.withDefaults());
 
@@ -33,5 +54,12 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
 
+        return configuration.getAuthenticationManager();
+
+    }
 }
